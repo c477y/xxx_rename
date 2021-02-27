f83fcedd4eb41f7a2058adbff0035d04cdf102d1
c477y <c477y@pm.me>
2021-02-27 21:25:29 +0000

    Join two filename generation methods

--- lib/xxx_rename/scene_by_file/brazzers.rb
+++ lib/xxx_rename/scene_by_file/brazzers.rb
@@ -7,35 +7,15 @@ module XxxRename
         super("https://www.brazzers.com/home")
       end
 
-      # Remove the following characters from the generated file name:
-      #
-      # . (dot)
-      # U+0000 (NUL)
-      # / (slash)
-      # \ (backslash)
-      # : (colon)
-      # * (asterisk)
-      # ? (question mark)
-      # " (quote)
-      # < (less than)
-      # > (greater than)
-      # | (pipe)
-      #
       def create_file_name(hsh, file)
         file_name = ""
         file_name += "#{hsh[:title]} "
         file_name += "[C] #{hsh[:collection]} " unless hsh[:collection].nil?
         file_name += "[F] #{hsh[:female_actors].join(", ")} "
-        file_name += "[M] #{hsh[:male_actors].join(", ")}" unless hsh[:male_actors].empty?
+        file_name += "[M] #{hsh[:male_actors].join(", ")}" if file_name.length < 150 && !hsh[:male_actors].empty?
         file_name.gsub(%r{[.\x00/\\:*?!"<>|]}, "") + File.extname(file)
       end
 
-      def create_alternate_filename(hsh, file)
-        file_name = ""
-        file_name += "#{hsh[:title]} "
-        file_name += "[F] #{hsh[:female_actors].join(", ")}"
-        file_name.gsub(%r{[.\x00/\\:*?!"<>|]}, "") + File.extname(file)
-      end
     end
   end
 end
--- lib/xxx_rename/scene_by_file/digital_playground.rb
+++ lib/xxx_rename/scene_by_file/digital_playground.rb
@@ -7,27 +7,14 @@ module XxxRename
         super("https://www.digitalplayground.com/home")
       end
 
-      # Remove the following characters from the generated file name:
-      #
-      # . (dot)
-      # U+0000 (NUL)
-      # / (slash)
-      # \ (backslash)
-      # : (colon)
-      # * (asterisk)
-      # ? (question mark)
-      # " (quote)
-      # < (less than)
-      # > (greater than)
-      # | (pipe)
-      #
       def create_file_name(hsh, file)
         file_name = ""
         file_name += "#{hsh[:title]} "
         file_name += "[F] #{hsh[:female_actors].join(", ")} "
-        file_name += "[M] #{hsh[:male_actors].join(", ")}" unless hsh[:male_actors].empty?
+        file_name += "[M] #{hsh[:male_actors].join(", ")}" if file_name.length < 150 && !hsh[:male_actors].empty?
         file_name.gsub(%r{[.\x00/\\:*?!"<>|]}, "") + File.extname(file)
       end
+
     end
   end
 end
