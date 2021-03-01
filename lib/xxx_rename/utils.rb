92228ffc4cbd3d17790b21caac799a04e175ca6b
c477y <c477y@pm.me>
2021-03-01 01:10:19 +0000

    Strip final filename

--- lib/xxx_rename/utils.rb
+++ lib/xxx_rename/utils.rb
@@ -69,7 +69,7 @@ module XxxRename
         # Add male actors only if the file name has not exceeded 150 characters
         # This is done to handle cases where the filename can become too big and result in errors
         file_name += "[M] #{hsh[:male_actors].join(", ")}" if file_name.length < 150 && !hsh[:male_actors].empty?
-        file_name.gsub(%r{[.\x00/\\:*?!"<>|']}, "") + File.extname(file)
+        file_name.gsub(%r{[.\x00/\\:*?!"<>|']}, "").strip + File.extname(file)
       end
     end
   end
