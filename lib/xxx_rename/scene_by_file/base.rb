12488f0a2ea09295e9ace97ea3d505c80f006f76
c477y <c477y@pm.me>
2021-03-03 17:11:59 +0000

    Handle banned search errors

--- lib/xxx_rename/scene_by_file/base.rb
+++ lib/xxx_rename/scene_by_file/base.rb
@@ -14,10 +14,7 @@ module XxxRename
         @instance_token = XxxRename::NetworkHelper.refresh_token_mg(site_url)
       end
 
-      def search(file, limit)
-        scene_title_arr = XxxRename::Utils.scene_name(file).split("-")
-        search_string = XxxRename::Utils.adjust_apostrophe(scene_title_arr)
-
+      def search(search_string, limit)
         opts = opt_search_scene(search_string, limit)
         response = self.class.get(SEARCH_ENDPOINT, opts)
 
@@ -27,17 +24,46 @@ module XxxRename
         when 200
           parsed_response = JSON.parse(response.body)
           XxxRename::NetworkHelper.fetch_scene_details(parsed_response["result"])
+        when 404
+          parsed_response = JSON.parse(response.body)
+          if parsed_response.is_a?(Array) && banned_search?(parsed_response.first["errors"])
+            reduced_search_string = reduce_search_string(search_string)
+            print "Got banned search error with search string \
+#{search_string.to_s.colorize(:red)}. Trying with \
+#{reduced_search_string.to_s.colorize(:red)}\n"
+            search(reduced_search_string, 25)
+          else
+            obj = {
+              request_options: opts,
+              response_code: response.code,
+              response_body: response.body
+            }
+            raise SearchError.new(search_string, obj)
+          end
         else
+          # binding.pry
           obj = {
             request_options: opts,
             response_code: response.code,
             response_body: response.body
           }
-          raise SearchError.new(file, obj)
+          raise SearchError.new(search_string, obj)
         end
       end
 
       private
+
+      # Removes last alphabet from the search string
+      def reduce_search_string(search_string)
+        search_string.chop
+      end
+
+      def banned_search?(errors)
+        errors.each do |err|
+          return true if err["code"] == 1750
+        end
+        false
+      end
 
       def opt_search_scene(scene_name, limit)
         {
