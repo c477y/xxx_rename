e0c77f5ee63f0e66eea8ddd7441df3683d875e1f
c477y <c477y@pm.me>
2021-03-03 18:29:04 +0000

    Simplify processed file  lookup

--- lib/xxx_rename/processed_file.rb
+++ lib/xxx_rename/processed_file.rb
@@ -5,33 +5,21 @@ module XxxRename
     end
 
     def female_actors
-      @basename
-        .split("[F]")
-        .last
-        .split("[M]")
-        .first
-        .to_s
-        .split(",")
-        .map(&:strip)
-        .sort
+      @basename.split("[F]").last.split("[M]").first.to_s.split(",").map(&:strip).sort
     end
 
-    # TODO: Split on [C] collection
     def male_actors
-      @basename
-        .split("[M]")
-        .last
-        .to_s
-        .split(",")
-        .map(&:strip)
-        .sort
+      @basename.split("[M]").last.to_s.split(",").map(&:strip).sort
     end
 
     def title
-      @basename
-        .split("[F]")
-        .first
-        .strip
+      @basename.split("[F]").first.split("[C]").first.strip
+    end
+
+    def collection
+      return nil unless @basename.include?("[C]")
+
+      @basename.split("[F]").first.split("[C]").last.strip
     end
   end
 end
